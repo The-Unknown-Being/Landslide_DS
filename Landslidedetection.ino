3f3ab66409c609eec78d01586600cc4a96b074d0
Unknown <118769462+The-Unknown-Being@users.noreply.github.com>
2025-01-28 03:37:06 +0545

    Add files via upload

---
+++ Landslidedetection.ino
@@ -0,0 +1,98 @@
+#include <Wire.h> 
+#include <string.h>
+#include <LiquidCrystal_I2C.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+int soilMoistureSensor_pin = A0;
+int trans_pin_0 = 7;
+int soilMoistureValue = 0;
+int lowerThreshold = 0;
+int upperThreshold = 1023;
+int turn = 1;
+int curr = 0;
+
+void pwd(char* txt, int time) // prints txt and delays by time seconds
+{
+  lcd.clear();
+  lcd.print(txt);
+  delay(time*1000);
+  lcd.clear();
+}
+
+void lcdInit()
+{
+  lcd.begin();
+  lcd.backlight();
+  lcd.setBacklight(50);
+}
+
+void caliberate()
+{
+  pwd("Initializing...", 1);
+
+  // when sensor is in air
+  turn = 1;
+  pwd("In dry air...", 2);
+  lcd.print("Now caliberating...");
+  int caliberatingValue = lowerThreshold;
+  int temp = 0;
+  for(int i = 0; i < 30; i++)
+  {
+    delay(100);
+    temp = analogRead(soilMoistureSensor_pin);
+    if( temp > caliberatingValue)
+    {
+      caliberatingValue = temp;
+    }
+  }
+  upperThreshold = caliberatingValue;
+  pwd("Caliberated!!", 1);
+
+  // when sensor is in water
+  pwd("In water...", 2);
+  lcd.print("Now caliberating...");
+  caliberatingValue = upperThreshold;
+  for(int i = 0; i < 30; i++)
+  {
+    delay(100);
+    temp = analogRead(soilMoistureSensor_pin);
+    if(temp < caliberatingValue)
+    {
+      caliberatingValue = temp;
+    }
+  }
+
+  lowerThreshold = caliberatingValue;
+  pwd("Caliberated!!", 1);
+}
+
+
+void setup()
+{
+  pinMode(soilMoistureSensor_pin, INPUT_PULLUP);
+  pinMode(trans_pin_0, OUTPUT);
+  digitalWrite(trans_pin_0, HIGH );
+
+  Serial.begin(9600);
+	lcdInit();
+  caliberate();
+
+  Serial.println(upperThreshold);
+  Serial.println(lowerThreshold);
+}
+
+void loop()
+{
+  soilMoistureValue = analogRead(soilMoistureSensor_pin);
+  curr = abs(float(upperThreshold-soilMoistureValue) / float(upperThreshold-lowerThreshold) * 100);
+  String lcdTxt = "Soil: " + String(curr) + " %";
+  lcdTxt = lcdTxt.c_str();
+  lcd.print(lcdTxt);
+  delay(1000);
+  lcd.clear();
+}
+
+ 
+
